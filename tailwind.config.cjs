485766b1b755750397cf1668bad18dd8b2f180a8
lnowl <102725713+lnowl@users.noreply.github.com>
2025-09-11 01:12:03 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Kanit', 'sans-serif'], // ตั้ง Kanit เป็น default font
+      },
+    },
   },
   plugins: [],
 }
